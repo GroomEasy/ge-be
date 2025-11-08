302f73da96b21a032155e212636c0bba10050aea
이지원 <daggero@naver.com>
2025-11-08 13:18:54 +0900

    refactor: GroomEasy migration to Menual

--- src/main/java/com/groomeasy/backend/MenualApplication.java
+++ src/main/java/com/groomeasy/backend/MenualApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GroomEasyApplication {
+public class MenualApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GroomEasyApplication.class, args);
+        SpringApplication.run(MenualApplication.class, args);
     }
 
 }

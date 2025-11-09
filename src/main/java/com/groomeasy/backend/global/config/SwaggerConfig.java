a8d4a963962019f988aebf2b795a57095a1d481b
hayong39 <yia0524_hy@naver.com>
2025-11-09 15:04:35 +0900

    [GE-36] chore: swagger 설정 추가

---
+++ src/main/java/com/groomeasy/backend/global/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.groomeasy.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.components(new Components()
+				.addSecuritySchemes("Authorization", new SecurityScheme()
+					.type(SecurityScheme.Type.HTTP)
+					.scheme("bearer")
+					.in(SecurityScheme.In.HEADER)
+					.bearerFormat("JWT")
+				)
+			)
+			.addSecurityItem(new SecurityRequirement().addList("Authorization"))
+			.info(new Info()
+				.title("MENUAL API")
+				.description("MENUAL API 명세서")
+				.version("1.0.0")
+			);
+	}
+
+}

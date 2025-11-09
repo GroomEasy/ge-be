b136275ac98f1b80a0cd0ce66242bd1fd9c08c7a
이지원 <daggero@naver.com>
2025-11-10 00:51:47 +0900

    [GE-38] feat: JwtAuthenticationFilter 클래스 작성

--- src/main/java/com/ceos/menual/global/config/jwt/JwtProvider.java
+++ src/main/java/com/ceos/menual/global/config/jwt/JwtProvider.java
@@ -1,10 +1,9 @@
 package com.ceos.menual.global.config.jwt;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
+
 import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -20,9 +19,11 @@ public class JwtProvider {
     @Value("${jwt.secret}")
     private String secretKey;
 
+    @Getter
     @Value("${jwt.access-token-validity}")
     private long accessTokenValidity;
 
+    @Getter
     @Value("${jwt.refresh-token-validity}")
     private long refreshTokenValidity;
 
@@ -67,62 +68,6 @@ public class JwtProvider {
                 .expiration(validity)
                 .signWith(key)
                 .compact();
-    }
-
-    // 사용자 ID 추출
-    public Long getUserIdFromToken(String token) {
-        return Long.parseLong(
-                Jwts.parser()
-                        .verifyWith(key)
-                        .build()
-                        .parseSignedClaims(token)
-                        .getPayload()
-                        .getSubject()
-        );
-    }
-
-    // 토큰 검증
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parser()
-                    .verifyWith(key)
-                    .build()
-                    .parseSignedClaims(token);
-            return true;
-        } catch (ExpiredJwtException e) {
-            log.warn("만료된 JWT 토큰입니다.");
-            throw e;
-        } catch (UnsupportedJwtException e) {
-            log.warn("지원되지 않는 JWT 토큰입니다.");
-            throw e;
-        } catch (MalformedJwtException e) {
-            log.warn("잘못된 JWT 토큰입니다.");
-            throw e;
-        } catch (SecurityException e) {
-            log.warn("잘못된 JWT 서명입니다.");
-            throw e;
-        } catch (IllegalArgumentException e) {
-            log.warn("JWT 토큰이 잘못되었습니다.");
-            throw e;
-        }
-    }
-
-    // 토큰 타입 추출
-    public String getTokenType(String token) {
-        return Jwts.parser()
-                .verifyWith(key)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload()
-                .get("type", String.class);
-    }
-
-    public long getAccessTokenValidity() {
-        return accessTokenValidity;
-    }
-
-    public long getRefreshTokenValidity() {
-        return refreshTokenValidity;
     }
 
 }

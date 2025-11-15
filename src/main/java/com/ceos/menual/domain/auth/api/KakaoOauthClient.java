7807a429d7382dc353293f8a39aae8b916d6e12d
hayong39 <yia0524_hy@naver.com>
2025-11-15 20:19:23 +0900

    [GE-30] fix: 예외 처리 추가

--- src/main/java/com/ceos/menual/domain/auth/api/KakaoOauthClient.java
+++ src/main/java/com/ceos/menual/domain/auth/api/KakaoOauthClient.java
@@ -5,8 +5,11 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 
 import com.ceos.menual.domain.auth.dto.response.KakaoUserResponseDTO;
+import com.ceos.menual.domain.auth.exception.AuthErrorCode;
+import com.ceos.menual.global.exception.GlobalException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,27 +29,47 @@ public class KakaoOauthClient {
 	private final WebClient kakaoApiWebClient;
 
 	public String getAccessToken(String code) {
-		return kakaoAuthWebClient.post()
-			.uri(uriBuilder -> uriBuilder
-				.path("/oauth/token")
-				.queryParam("grant_type", "authorization_code")
-				.queryParam("client_id", clientId)
-				.queryParam("redirect_uri", redirectUri)
-				.queryParam("code", code)
-				.build())
-			.retrieve()
-			.bodyToMono(Map.class)
-			.map(res -> (String)res.get("access_token"))
-			.block();
+		try {
+			return kakaoAuthWebClient.post()
+				.uri(uriBuilder -> uriBuilder
+					.path("/oauth/token")
+					.queryParam("grant_type", "authorization_code")
+					.queryParam("client_id", clientId)
+					.queryParam("redirect_uri", redirectUri)
+					.queryParam("code", code)
+					.build())
+				.retrieve()
+				.bodyToMono(Map.class)
+				.map(res -> {
+					String accessToken = (String) res.get("access_token");
+					if (accessToken == null) {
+						throw new GlobalException(AuthErrorCode.KAKAO_TOKEN_ERROR);
+					}
+					return accessToken;
+				})
+				.block();
+		} catch (WebClientRequestException e){
+			throw new GlobalException(AuthErrorCode.KAKAO_TOKEN_ERROR);
+		}
 	}
 
 	public KakaoUserResponseDTO getUserInfo(String accessToken){
-		return kakaoApiWebClient.get()
-			.uri("/v2/user/me")
-			.headers(headers -> headers.setBearerAuth(accessToken))
-			.retrieve()
-			.bodyToMono(KakaoUserResponseDTO.class)
-			.block();
+		try {
+			KakaoUserResponseDTO userInfo = kakaoApiWebClient.get()
+				.uri("/v2/user/me")
+				.headers(headers -> headers.setBearerAuth(accessToken))
+				.retrieve()
+				.bodyToMono(KakaoUserResponseDTO.class)
+				.block();
+
+			if (userInfo == null){
+				throw new GlobalException(AuthErrorCode.KAKAO_USER_INFO_ERROR);
+			}
+			return userInfo;
+
+		}catch (WebClientRequestException e){
+			throw new GlobalException(AuthErrorCode.KAKAO_USER_INFO_ERROR);
+		}
 	}
 
 }

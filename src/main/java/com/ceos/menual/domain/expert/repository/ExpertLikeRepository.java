77fe86afeb4ddea36315168e6e07b020468f1f87
이지원 <daggero@naver.com>
2026-01-04 03:29:13 +0900

    [GE-71] feat: 전문가 찜 추가 및 삭제 구현

--- src/main/java/com/ceos/menual/domain/expert/repository/ExpertLikeRepository.java
+++ src/main/java/com/ceos/menual/domain/expert/repository/ExpertLikeRepository.java
@@ -1,6 +1,8 @@
 package com.ceos.menual.domain.expert.repository;
 
 import com.ceos.menual.entity.ExpertLike;
+import com.ceos.menual.entity.ExpertProfile;
+import com.ceos.menual.entity.GeneralProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,7 +12,8 @@ public interface ExpertLikeRepository extends JpaRepository<ExpertLike, Long> {
     // 전문가 찜 API 용
     boolean existsByGeneralProfileIdAndExpertProfileId(Long generalProfileId, Long expertProfileId);
 
-    Optional<ExpertLike> findByGeneralProfileIdAndExpertProfileId(Long generalProfileId, Long expertProfileId);
+    Boolean existsByGeneralProfileAndExpertProfile(GeneralProfile generalProfile, ExpertProfile expertProfile);
+    Optional<ExpertLike> findByGeneralProfileAndExpertProfile(GeneralProfile generalProfile, ExpertProfile expertProfile);
 
     long countByExpertProfileId(Long expertProfileId);
 }

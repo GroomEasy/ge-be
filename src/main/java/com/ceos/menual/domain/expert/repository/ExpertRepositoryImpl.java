f71d486f76f35d75239b3a9b07849b9580e7b453
이지원 <daggero@naver.com>
2026-01-14 20:06:12 +0900

    [GE-91] feat: 전문가 찜 조회 API authenticated 추가

--- src/main/java/com/ceos/menual/domain/expert/repository/ExpertRepositoryImpl.java
+++ src/main/java/com/ceos/menual/domain/expert/repository/ExpertRepositoryImpl.java
@@ -61,7 +61,6 @@ public class ExpertRepositoryImpl implements ExpertRepository {
 
 	@Override
 	public List<ExpertRankingResponseDTO> findTop3ByCategory(Category category, Long currentUserId) {
-		QExpertLike el = QExpertLike.expertLike;
 
 		return queryFactory
 				.select(Projections.constructor(

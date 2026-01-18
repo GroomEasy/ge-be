31c662dd3c1e3e99159b8a41b1e658a6adaee2c2
mirupio <162397816+mirupio@users.noreply.github.com>
2026-01-18 17:13:33 +0900

    [GE-88] refactor: experterrorcode 충돌 해결

--- src/main/java/com/ceos/menual/domain/expert/exception/ExpertErrorCode.java
+++ src/main/java/com/ceos/menual/domain/expert/exception/ExpertErrorCode.java
@@ -16,7 +16,8 @@ public enum ExpertErrorCode  implements ResultCode {
     LIKE_NOT_FOUND(HttpStatus.CONFLICT, 6005, "해당 전문가에 대한 찜을 찾을 수 없습니다."),
     PORTFOLIO_NOT_FOUND(HttpStatus.NOT_FOUND, 6006, "포트폴리오를 찾을 수 없거나 본인의 포트폴리오가 아닙니다."),
     NO_REPRESENTATIVE_PORTFOLIO(HttpStatus.NOT_FOUND, 6007, "해제할 대표 포트폴리오가 없습니다."),
-    INVALID_IMAGE_KEY_FORMAT(HttpStatus.BAD_REQUEST, 6008, "잘못된 이미지 키 형식입니다.");
+    NO_EXPERT_BANK_ACCOUNT(HttpStatus.NOT_FOUND, 6008, "해당 전문가의 계좌가 존재하지 않습니다."),
+    INVALID_IMAGE_KEY_FORMAT(HttpStatus.BAD_REQUEST, 6009, "잘못된 이미지 키 형식입니다.");
 
 
 
